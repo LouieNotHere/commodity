74df068c210a66f58434f8bdef9471bd248a67aa
Louie Soulen Kurenai <168345503+LouieNotHere@users.noreply.github.com>
2025-02-28 22:35:14 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,134 +1,71 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, Modal, Vault, TFile } from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class VaultValuePlugin extends Plugin {
 	async onload() {
-		await this.loadSettings();
+		console.log("VaultValuePlugin loaded");
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		this.addRibbonIcon("lucide-calculator", "Calculate Vault Value", async () => {
+			const vaultStats = await calculateVaultStats(this.app.vault);
+			new VaultValueModal(this.app, vaultStats).open();
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
+		console.log("VaultValuePlugin unloaded");
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class VaultValueModal extends Modal {
+	private stats: VaultStats;
+
+	constructor(app: App, stats: VaultStats) {
 		super(app);
+		this.stats = stats;
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+		contentEl.empty();
+
+		const smallHeader = contentEl.createEl("h4", { text: "Vault Value" });
+
+		const vaultValue = calculateVaultValue(this.stats);
+		const bigHeader = contentEl.createEl("h1", { text: vaultValue.toFixed(2) });
 	}
 
 	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+		this.contentEl.empty();
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+interface VaultStats {
+	totalCharacters: number;
+	totalWords: number;
+	totalFiles: number;
+	totalSentences: number;
+}
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
+async function calculateVaultStats(vault: Vault): Promise<VaultStats> {
+	let totalCharacters = 0;
+	let totalWords = 0;
+	let totalFiles = 0;
+	let totalSentences = 0;
+
+	const files = vault.getMarkdownFiles();
+	totalFiles = files.length;
+
+	for (const file of files) {
+		const content = await vault.read(file);
+		totalCharacters += content.length;
+		totalWords += content.split(/\s+/).length;
+		totalSentences += (content.match(/[.!?]+/g) || []).length;
 	}
 
-	display(): void {
-		const {containerEl} = this;
+	return { totalCharacters, totalWords, totalFiles, totalSentences };
+}
 
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+// Function to compute the Vault Value using the given formula
+function calculateVaultValue(stats: VaultStats): number {
+	const { totalCharacters: a, totalWords: b, totalFiles: c, totalSentences: d } = stats;
+	return (a / 122000) * (1 + (b / 130000)) + (c / 200) + (d / 21000);
 }

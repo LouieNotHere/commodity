f68755c0f377664fb88752194b81e90068f525cc
Louie Soulen Kurenai <168345503+LouieNotHere@users.noreply.github.com>
2025-03-01 17:34:19 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@
 // I deeply apologize for that, I am just trying to add some new things to the source code.
 
 import { Modal, App } from "obsidian";
-import { VaultStats } from "./types/vaultStats.js"; // Strict check just in case the compilation fails
+import { VaultStats } from "./types/vaultStats";
 
 export class VaultValueModal extends Modal {
     stats: VaultStats;

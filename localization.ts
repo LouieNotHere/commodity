f9c779fe39203d17b264b30c6bed813b6057e0f7
Louie Soulen Kurenai <168345503+LouieNotHere@users.noreply.github.com>
2025-03-08 07:15:23 +0800

    Update localization.ts

--- localization.ts
+++ localization.ts
@@ -18,18 +18,18 @@
 export function getLocalizedText(key: string, language: string): string {
   const LOCALIZED_STRINGS: Record<string, Record<string, string>> = {
     "ribbonTooltip": {
-      "en": "Commodity: Calculate Vault Value",
+      "en": "Commodity: Calculate vault value",
       "ja": "Commodity：ボールトの価値を計算",
-      "id": "Commodity: Kalkulasi Nilai Berangkas",
+      "id": "Commodity: Kalkulasi nilai berangkas",
       "vi": "Commodity: Tính giá trị của ví",
       "es": "Commodity: Calcular el Valor de la Bóveda"
     },
     "modalTitle": {
-      "en": "Calculated Vault Value:",
+      "en": "Calculated vault value:",
       "ja": "計算されたボールトの価値:",
-      "id": "Nilai Kalkulasi Berangkas:",
+      "id": "Nilai kalkulasi berangkas:",
       "vi": "Giá trị của ví đã tính được:",
-      "es": "Valor Calculado de la Bóveda:"
+      "es": "Valor calculado de la bóveda:"
     },
     "calculatingNotice": {
       "en": "Commodity: Calculating the vault value...",
@@ -39,18 +39,18 @@ export function getLocalizedText(key: string, language: string): string {
       "es": "Commodity: Calculando el Valor de la Bóveda..."
     },
     "calculatedTime": {
-      "en": "Total CPU Time:",
+      "en": "Total CPU time:",
       "ja": "合計CPU時間:",
-      "id": "Total Waktu CPU:",
+      "id": "Total waktu CPU:",
       "vi": "Tổng thời gian CPU:",
-      "es": "Tiempo de CPU en Total:"
+      "es": "Tiempo de CPU en total:"
     },
     "currencySetting": {
-      "en": "Currency Preference",
+      "en": "Currency preference",
       "ja": "通貨の設定",
-      "id": "Preferensi Mata Uang",
+      "id": "Preferensi mata uang",
       "vi": "Đơn vị tiền ảo",
-      "es": "Moneda a Usar"
+      "es": "Moneda a usar"
     },
     "currencyDescription": {
       "en": "Select the preferred currency that can be used for the value calculation",
@@ -60,11 +60,11 @@ export function getLocalizedText(key: string, language: string): string {
       "es": "Seleccione la moneda que desea usar para el cálculo de la bóveda."
     },
     "languageSetting": {
-      "en": "Language Preference",
+      "en": "Language preference",
       "ja": "言語の設定",
-      "id": "Preferensi Bahasa",
+      "id": "Preferensi bahasa",
       "vi": "Ngôn ngữ",
-      "es": "Idioma a Usar"
+      "es": "Idioma a usar"
     },
     "languageDescription": {
       "en": "Select the preferred language for the plugin interface",
@@ -74,18 +74,18 @@ export function getLocalizedText(key: string, language: string): string {
       "es": "Seleccione el idioma a usar para la interfaz del plugin"
     },
 	"sidebarRibbonTitle": {
-  	  "en": "Commodity: View Active Note Value",
+  	  "en": "Commodity: View active note value",
       "ja": "Commodity: アクティブノートの価値を表示",
-   	  "id": "Commodity: Lahat Cacatan Nilai Aktif",
-  	  "vi": "Commodity: View Active Note Value",
-	  "es": "Commodity: Ver Valor de la Nota Actual"
+   	  "id": "Commodity: Lahat cacatan nilai aktif",
+  	  "vi": "Commodity: View active note value",
+	  "es": "Commodity: Ver valor de la nota actual"
 	},
     "sidebarTitle": {
-	  "en": "Active Note Value",
+	  "en": "Active note value",
 	  "ja": "アクティブノートの価値",
-	  "id": "Cacatan Nilai Aktif",
-	  "vi": "Active Note Value",
-	  "es": "Valor de la Nota Actual"
+	  "id": "Cacatan nilai aktif",
+	  "vi": "Active note nalue",
+	  "es": "Valor de la nota actual"
     },
     "sidebarLoading": {
 	  "en": "Loading the value of the active note...",
@@ -102,11 +102,11 @@ export function getLocalizedText(key: string, language: string): string {
 	  "es": "No tienes ninguna nota actualmente."
 	},
 	"noteValue": {
-      "en": "Note Value:",
+      "en": "Note value:",
 	  "ja": "ノートの価値:",
-	  "id": "Nilai Catatan:",
-	  "vi": "Note Value:",
-	  "es": "Valor de la Nota:"
+	  "id": "Nilai catatan:",
+	  "vi": "Note value:",
+	  "es": "Valor de la nota:"
 	}
   };
 

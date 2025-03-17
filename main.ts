6837a627548009be2cd17cbc107cbdf03990d244
Louie Soulen Kurenai <168345503+LouieNotHere@users.noreply.github.com>
2025-03-17 23:09:38 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -27,7 +27,7 @@ export default class CommodityPlugin extends Plugin {
       console.log("Commodity Plugin Loaded");
 
   await this.loadSettings();
-  this.language = this.app.getLanguage();
+  this.language = "this.settings.language || "en";
   this.addSettingTab(new CommoditySettingsTab(this.app, this));
 
   this.addRibbonIcon(

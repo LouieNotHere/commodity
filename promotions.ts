274c44f308e8d1ea70e24a998874c03ad4f83db1
Louie Soulen Kurenai <168345503+LouieNotHere@users.noreply.github.com>
2025-03-22 19:25:31 +0800

    Create promotions.ts

---
+++ promotions.ts
@@ -0,0 +1,42 @@
+import { App, PluginSettingTab } from "obsidian";
+import { getLocalizedText } from "./localization";
+
+export class CommodityPromotionsTab extends PluginSettingTab {
+  plugin: any;
+
+  this.language = this.settings.language || "en";
+
+  constructor(app: App, plugin: any) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
+
+  display(): void {
+    const { containerEl } = this;
+    containerEl.empty();
+
+    containerEl.createEl("h2", { text: getLocalizedText("supportText", this.language) });
+
+    containerEl.createEl("p", {
+      text: getLocalizedText("promotionText", this.language);
+    });
+
+    const linkEl = containerel.createEl("a", {
+      attr: {
+        href: "https://ko-fi.com/paytouse1774",
+        target: "_blank"
+      }
+    }
+
+    const imageEl = linkEl.createEl("img", {
+      attr: {
+        src: "https://storage.ko-fi.com/cdn/kofi5.png?v=6",
+        alt: "Ko-fi",
+      },
+    });
+
+    imageEl.style.width = "100%";
+    imageEl.style.borderRadius = "8px";
+    imageEl.style.marginTop = "10px";
+  }
+}

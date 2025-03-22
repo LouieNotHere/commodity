57d01caaa450f40921b4d0e0a9da3e9e08c3c627
Louie Soulen Kurenai <168345503+LouieNotHere@users.noreply.github.com>
2025-03-22 17:32:59 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -17,91 +17,114 @@ import { CommoditySidebarView, VIEW_TYPE_COMMODITY } from "./views/SidebarView";
 export default class CommodityPlugin extends Plugin {
   settings: CommoditySettings;
   language: string;
-
+  
   async onload() {
-	  this.registerView(
-          VIEW_TYPE_COMMODITY,
-		  (leaf) => new CommoditySidebarView(leaf, this)
-	  );
-	  
-      console.log("Commodity Plugin Loaded");
-
-  await this.loadSettings();
-  this.language = this.settings.language || "en";
-  this.addSettingTab(new CommoditySettingsTab(this.app, this));
-
-  this.addCommand({
-    id: "calculate-vault-value",
-    name: "Calculate Vault Value",
-    callback: async () => {
+    this.registerView(
+      VIEW_TYPE_COMMODITY,
+      (leaf) => new CommoditySidebarView(leaf, this)
+    );
+    
+    console.log("Commodity Plugin Loaded");
+    
+    await this.loadSettings();
+    this.language = this.settings.language || "en";
+    this.addSettingTab(new CommoditySettingsTab(this.app, this));
+    
+    this.addCommand({
+      id: "calculate-vault-value",
+      name: "Calculate Vault Value",
+      callback: async () => {
         const vaultStats = await calculateVaultStats(this.app.vault);
         const vaultValue = await calculateVaultValue(vaultStats, this.settings.currency, this.app.vault);
         new VaultValueModal(this.app, vaultValue, this.settings.currency, this.language).open();
-    },
-    hotkeys: [
-        {
-            modifiers: ["Mod", "Shift"],
-            key: "V",
-        },
-    ],
-});
-
-  this.addRibbonIcon(
-    "lucide-calculator",
-    getLocalizedText("ribbonTooltip", this.language),
-    async () => {
-      const vaultStats = await calculateVaultStats(this.app.vault);
-      const vaultValue = await calculateVaultValue(vaultStats, this.settings.currency, this.app.vault);
-      new VaultValueModal(this.app, vaultValue, this.settings.currency, this.language).open();
-    }
-  );
-
-  this.addCommand({
-    id: "activate-commodity-sidebar",
-    name: "Open Commodity Sidebar",
-    callback: async () => await this.activateView(),
-    hotkeys: [
-        {
-            modifiers: ["Mod", "Shift"],
-            key: "B",
-        },
-    ],
-});
-
-  this.addRibbonIcon(
-    "lucide-dollar-sign",
-	getLocalizedText("sidebarRibbonTitle", this.language),
-    async () => {
-	  await this.activateView();
-	});
+      },
+      hotkeys: [
+      {
+        modifiers: ["Mod", "Shift"],
+        key: "V",
+      }, ],
+    });
+    
+    this.addRibbonIcon(
+      "lucide-calculator",
+      getLocalizedText("ribbonTooltip", this.language),
+      async () => {
+        const vaultStats = await calculateVaultStats(this.app.vault);
+        const vaultValue = await calculateVaultValue(vaultStats, this.settings.currency, this.app.vault);
+        new VaultValueModal(this.app, vaultValue, this.settings.currency, this.language).open();
+      }
+    );
+    
+    this.addCommand({
+      id: "calculate-vault-value-reworked",
+      name: "Commodity: Calculate Vault Value (Reworked)",
+      callback: async () => {
+        const vaultStats = await calculateVaultStats(this.app.vault);
+        const vaultValue = await calculateReworkedValue(vaultStats, this.settings.currency, this.app.vault);
+        new VaultValueModal(this.app, vaultValue, this.settings.currency, this.language).open();
+      },
+      hotkeys: [
+      {
+        modifiers: ["Mod", "Shift"],
+        key: "R",
+      }, ],
+    });
+    
+    this.addRibbonIcon(
+      "lucide-coins",
+      getLocalizedText("ribbonReworkedTooltip", this.language),
+      async () => {
+        const vaultStats = await calculateVaultStats(this.app.vault);
+        const vaultValue = await calculateReworkedValue(vaultStats, this.settings.currency, this.app.vault);
+        new VaultValueModal(this.app, vaultValue, this.settings.currency, this.language).open();
+      }
+    );
+    
+    this.addCommand({
+      id: "activate-commodity-sidebar",
+      name: "Open Commodity Sidebar",
+      callback: async () => await this.activateView(),
+      hotkeys: [
+      {
+        modifiers: ["Mod", "Shift"],
+        key: "B",
+      }, ],
+    });
+    
+    this.addRibbonIcon(
+      "lucide-dollar-sign",
+      getLocalizedText("sidebarRibbonTitle", this.language),
+      async () => {
+        await this.activateView();
+      });
   }
-
+  
   async activateView() {
     const { workspace } = this.app;
-
+    
     let leaf = workspace.getLeavesOfType(VIEW_TYPE_COMMODITY)[0];
-
+    
     if (!leaf) {
-        const newLeaf = workspace.getRightLeaf(false);
-        if (!newLeaf) {
-            console.error("Failed to create a sidebar view: No available leaf.");
-            return;
-        }
-        leaf = newLeaf;
-
-        await leaf.setViewState({
-            type: VIEW_TYPE_COMMODITY,
-            active: true,
-        });
+      const newLeaf = workspace.getRightLeaf(false);
+      if (!newLeaf) {
+        console.error("Failed to create a sidebar view: No available leaf.");
+        return;
+      }
+      leaf = newLeaf;
+      
+      await leaf.setViewState({
+        type: VIEW_TYPE_COMMODITY,
+        active: true,
+      });
     }
-
+    
     workspace.revealLeaf(leaf);
   }
-
+  
   async loadSettings() {
     this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
   }
-
+  
   async saveSettings() {
     await this.saveData(this.settings);
   }
@@ -111,54 +134,54 @@ class VaultValueModal extends Modal {
   private vaultValue: number;
   private currency: string;
   private language: string;
-
+  
   constructor(app: App, vaultValue: number, currency: string, language: string) {
     super(app);
     this.vaultValue = vaultValue;
     this.currency = currency;
     this.language = language;
   }
-
+  
   onOpen() {
     new Notice(getLocalizedText("calculatingNotice", this.language));
-
+    
     const { contentEl } = this;
     contentEl.empty();
     contentEl.style.textAlign = "center";
     contentEl.style.fontFamily = "var(--default-font)";
-
+    
     const startTime = performance.now();
-
+    
     contentEl.createEl("h4", {
       text: getLocalizedText("modalTitle", this.language),
       cls: "window-header",
     });
-
+    
     const currencySymbol = getCurrencySymbol(this.currency);
     const endTime = performance.now();
     const timeTaken = (endTime - startTime).toFixed(2);
-
+    
     const formatter = new Intl.NumberFormat(this.language, { minimumFractionDigits: 0, maximumFractionDigits: 0 });
-
+    
     const fullValue = Number(this.vaultValue.toFixed(25));
     const truncatedValue = Math.trunc(fullValue);
     var formattedValue: string = formatter.format(truncatedValue);
-
+    
     var valueText: string = `${currencySymbol}${this.vaultValue.toFixed(2)}`;
-
+    
     if (this.vaultValue >= 1000000) {
       valueText = `${currencySymbol}${abbreviateNumber(truncatedValue)}`;
     } else if (this.vaultValue >= 1000) {
       valueText = `${currencySymbol}${formattedValue}`;
     }
-
+    
     contentEl.createEl("h1", { text: valueText, cls: "window-value" });
     contentEl.createEl("p", {
       text: `${getLocalizedText("calculatedTime", this.language)} ${timeTaken} ms`,
       cls: "window-time",
     });
   }
-
+  
   onClose() {
     this.contentEl.empty();
   }
@@ -171,54 +194,63 @@ interface VaultStats {
   totalSentences: number;
 }
 
-async function calculateVaultStats(vault: Vault): Promise<VaultStats> {
+async function calculateVaultStats(vault: Vault): Promise < VaultStats > {
   let totalCharacters = 0;
   let totalWords = 0;
   let totalFiles = 0;
   let totalSentences = 0;
-
+  
   const files = vault.getMarkdownFiles();
   totalFiles = files.length;
-
+  
   for (const file of files) {
     const content = await vault.read(file);
     totalCharacters += content.length;
     totalWords += content.split(/\s+/).length;
     totalSentences += (content.match(/[.!?]+/g) || []).length;
   }
-
+  
   return { totalCharacters, totalWords, totalFiles, totalSentences };
 }
 
-async function calculateVaultValue(stats: VaultStats, currency: string, vault: Vault): Promise<number> {
+async function calculateVaultValue(stats: VaultStats, currency: string, vault: Vault): Promise < number > {
   const { totalCharacters: a, totalWords: b, totalFiles: c, totalSentences: d } = stats;
   let value = (a / 122000) * (1 + (b / 130000)) + (c / 200) + (d / 21000);
-
+  
   const e = await getVaultAgeInDays(vault) / 60;
-
+  
   const finalValue = (value + e) * (CURRENCY_MULTIPLIERS[currency] || 1);
   return Number(finalValue.toFixed(50));
 }
 
-async function getVaultAgeInDays(vault: Vault): Promise<number> {
+async function calculateReworkedValue(stats: VaultStats, currency: string, vault: Vault): Promise < number > {
+  const { totalCharacters: a, totalWords: b, totalFiles: c, totalSentences: d } = stats;
+  const e = await getVaultAgeInDays(vault) / 30;
+  
+  let value = (a / 92500) * (1 + (b / 105000)) + (c / 50) + (d / 12250) + (e / 30);
+  
+  return Number((value * (CURRENCY_MULTIPLIERS[currency] || 1)).toFixed(50));
+}
+
+async function getVaultAgeInDays(vault: Vault): Promise < number > {
   try {
     const configFile = vault.getAbstractFileByPath(`${this.app.vault.configDir}/app.json`);
-
+    
     if (!configFile || !(configFile instanceof TFile)) {
       console.warn("Vault creation date file not found. Returning 0.");
       return 0;
     }
-
+    
     const stats = await vault.adapter.stat(configFile.path);
     if (!stats || stats.ctime === undefined) {
       console.warn("Could not retrieve vault creation date. Returning 0.");
       return 0;
     }
-
+    
     const creationTime = stats.ctime;
     const currentTime = Date.now();
     const daysSinceCreation = (currentTime - creationTime) / (1000 * 60 * 60 * 24);
-
+    
     return daysSinceCreation;
   } catch (error) {
     console.error("Error fetching vault creation date:", error);
@@ -227,7 +259,7 @@ async function getVaultAgeInDays(vault: Vault): Promise<number> {
 }
 
 function getCurrencySymbol(currency: string): string {
-  const symbols: Record<string, string> = {
+  const symbols: Record < string, string > = {
     "USD": "US$",
     "JPY": "JP¥",
     "PHP": "₱",
